05950b60da5ab3fb947ce47e0b7a0265e6d6e532
kyd <husheart@naver.com>
2020-10-04 00:46:04 +0900

    jpa-practical1 init

---
+++ src/main/java/tk/youngdk/jpashop/controller/TestConroller.java
@@ -0,0 +1,22 @@
+package tk.youngdk.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestConroller {
+
+    @GetMapping("test")
+    public String test(Model model){
+        moddel.add
+        log.error("test");
+
+        return "test";
+
+    }
+}
+
+7  c
