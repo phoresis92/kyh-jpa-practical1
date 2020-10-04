f94f737461a1f8443bca067fedb6780bf9685bb7
kyd <husheart@naver.com>
2020-10-05 02:40:15 +0900

    7 - 상품 주문
    웹 계층 개발

---
+++ src/main/java/tk/youngdk/jpashop/controller/OrderController.java
@@ -0,0 +1,45 @@
+package tk.youngdk.jpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tk.youngdk.jpashop.domain.Member;
+import tk.youngdk.jpashop.domain.item.Item;
+import tk.youngdk.jpashop.service.ItemService;
+import tk.youngdk.jpashop.service.MemberService;
+import tk.youngdk.jpashop.service.OrderServcie;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderServcie orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+
+    }
+
+    @PostMapping(value = "/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
