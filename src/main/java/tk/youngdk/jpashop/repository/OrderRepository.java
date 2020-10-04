bb1457d28b48a7e1028e0104cc368c53090cd3aa
kyd <husheart@naver.com>
2020-10-04 21:55:00 +0900

    6 - 주문 리포지토리 개발
    주문 도메인 개발

---
+++ src/main/java/tk/youngdk/jpashop/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package tk.youngdk.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import tk.youngdk.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long orderId) {
+        return em.find(Order.class, orderId);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch){
+//    }
+}
