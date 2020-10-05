7b0a13574e7747218cd78358ddf8b9dc515a4240
kyd <husheart@naver.com>
2020-10-06 02:57:38 +0900

    3 - 간단한 주문 조회 v4: JPA에서 DTO로 바로 조회
    API 개발 고급 - 지연 로딩과 조회 성능 최적화

--- src/main/java/tk/youngdk/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/tk/youngdk/jpashop/api/OrderSimpleApiController.java
@@ -1,16 +1,14 @@
 package tk.youngdk.jpashop.api;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tk.youngdk.jpashop.domain.Address;
 import tk.youngdk.jpashop.domain.Order;
-import tk.youngdk.jpashop.domain.OrderStatus;
 import tk.youngdk.jpashop.repository.OrderSearch;
+import tk.youngdk.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import tk.youngdk.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import tk.youngdk.jpashop.service.OrderServcie;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,12 +19,20 @@ import java.util.stream.Collectors;
  * Order (2건)
  * Order -> Member
  * Order -> Delivery
+ *
+ *
+ * 쿼리 방식 선택 권장 순서
+ * 1. 우선 엔티티를 DTO로 변환하는 방법을 선택한다.
+ * 2. 필요하면 페치 조인으로 성능을 최적화 한다. => 대부분의 성능 이슈가 해결된다.
+ * 3. 그래도 안되면 DTO로 직접 조회하는 방법을 사용한다.
+ * 4. 최후의 방법은 JPA가 제공하는 네이티브 SQL이나 스프링 JDBC Template을 사용해서 SQL을 직접 사용한다.
  * */
 @RestController
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
 
     private final OrderServcie orderServcie;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1(){
@@ -42,43 +48,32 @@ public class OrderSimpleApiController {
     }
 
     @GetMapping("/api/v2/simple-orders")
-    public List<SimpleOrderDto> ordersV2(){
+    public List<OrderSimpleQueryDto> ordersV2(){
         List<Order> orders = orderServcie.findOrders(new OrderSearch());
 
-        List<SimpleOrderDto> collect = orders.stream()
-                .map(order -> new SimpleOrderDto(order))
+        List<OrderSimpleQueryDto> collect = orders.stream()
+                .map(order -> new OrderSimpleQueryDto(order))
                 .collect(Collectors.toList());
 
         return collect;
     }
 
     @GetMapping("/api/v3/simple-orders")
-    public List<SimpleOrderDto> ordersV3(){
+    public List<OrderSimpleQueryDto> ordersV3(){
         List<Order> orders = orderServcie.findAllWithMemberDelivery();
 
-        List<SimpleOrderDto> collect = orders.stream()
-                .map(SimpleOrderDto::new)
+        List<OrderSimpleQueryDto> collect = orders.stream()
+                .map(OrderSimpleQueryDto::new)
                 .collect(Collectors.toList());
 
         return collect;
     }
 
-    @Data
-    static class SimpleOrderDto {
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4(){
+        List<OrderSimpleQueryDto> orders = orderSimpleQueryRepository.findOrderDtos();
 
-        public SimpleOrderDto(Order order) {
-            this.orderId = order.getId();
-            this.name = order.getMember().getName(); //Lazy 초기화
-            this.orderDate = order.getOrderDate();
-            this.orderStatus = order.getStatus();
-            this.address = order.getDelivery().getAddress(); //Lazy 초기화
-        }
+        return orders;
     }
-
 
 }
