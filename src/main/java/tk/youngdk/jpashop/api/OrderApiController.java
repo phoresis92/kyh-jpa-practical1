aaf361c8677c12528eaead305c320e7eb35d5604
kyd <husheart@naver.com>
2020-10-06 23:17:53 +0900

    4 - 주문 조회 v2: 엔티티를 DTO로 변환
    API 개발 고급 - 컬렉션 조회 최적화

--- src/main/java/tk/youngdk/jpashop/api/OrderApiController.java
+++ src/main/java/tk/youngdk/jpashop/api/OrderApiController.java
@@ -39,4 +39,55 @@ public class OrderApiController {
         return all;
     }
 
+    @GetMapping("/api/v2/orders")
+    public ReturnDto ordersV2(){
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+
+        List<OrderDto> collect = orders.stream()
+                .map(order -> new OrderDto(order))
+                .collect(Collectors.toList());
+
+        return new ReturnDto(collect, collect.size());
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order){
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+            this.orderItems = order.getOrderItems().stream()
+                .map(orderItem -> new OrderItemDto(orderItem))
+                .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName; //상품명
+        private int orderPrice; //주문 가
+        private int count; //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ReturnDto<T> {
+        T data;
+        int count;
+    }
 }
